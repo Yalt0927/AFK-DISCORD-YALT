7c7f140dbfd655c8aecedd1da439aad0d6616eb4
Yalt0927 <gjo173935@gmail.com>
2021-05-20 22:17:25 +0900

    bot commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { prefix, token } = require("./configj.son");
+
+
+client.on('ready', () => {
+    console.log('DISCORD BOT LOGIN');
+});
+
+
+client.on('message', (message) => {
+    if(message.content === 'Hello') {
+        message.reply('Hello, world!')
+    }
+});
+
+client.login(token);
