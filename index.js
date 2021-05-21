973452d8ff86cf7028a00efb2697e50659a8809d
Yalt0927 <gjo173935@gmail.com>
2021-05-21 22:13:55 +0900

    Yalt commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {prefix} = require("./config.json");
+const { prefix, token } = require("./config.json");
 
 
 client.on('ready', () => {
@@ -14,4 +14,4 @@ client.on('message', (message) => {
     }
 });
 
-client.login(process.env.TOKEN);
+client.login(token);
