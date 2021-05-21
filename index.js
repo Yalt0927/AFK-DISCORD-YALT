89f17722d1f728abf185a922b56d3061e098d0f6
Yalt0927 <gjo173935@gmail.com>
2021-05-21 21:53:10 +0900

    bot commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix, token } = require("./configj.son");
+const { prefix, token } = require("./config.json");
 
 
 client.on('ready', () => {
